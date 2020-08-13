b0e69b74599b0517889f77e762b84bed2b2fdcb5
jchuahtacc <jchuah@tacc.utexas.edu>
2020-08-13 17:50:32 -0500

    Allow random initiative for deployment

--- src/app/components/deploy/deploy.component.ts
+++ src/app/components/deploy/deploy.component.ts
@@ -8,12 +8,17 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class DeployComponent implements OnInit {
   initiative: number;
   @Output('onDeploy') onDeploy: EventEmitter<any> = new EventEmitter();
+  @Input('allowRandom') allowRandom: boolean = false;
   
   constructor() { }
 
   ngOnInit() {}
 
   deployClick() {
+    if (this.allowRandom && !this.initiative) {
+      this.initiative = Math.floor(Math.random() * 20) + 1;
+
+    }
     this.onDeploy.emit({ initiative: this.initiative });
   }
 
